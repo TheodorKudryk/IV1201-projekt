fe68972b5e4a22998a538f0d9395da0fd168f648
lleyou <61270505+lleyou@users.noreply.github.com>
2022-02-28 21:45:53 +0100

    transactions
    Transaction handling in server in repository and service classes.

--- Server/src/main/java/com/iv1201/server/repository/Competence_profileRepository.java
+++ Server/src/main/java/com/iv1201/server/repository/Competence_profileRepository.java
@@ -2,11 +2,18 @@ package com.iv1201.server.repository;
 
 import com.iv1201.server.entity.Competence_profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+@Repository
+@Transactional(propagation = Propagation.MANDATORY)
 /**
  *
  * @author theok
  */
 public interface Competence_profileRepository extends JpaRepository<Competence_profile,Integer> {
     
+    @Override
+    Competence_profile save(Competence_profile profile);
 }
