1f09156905e00fd31ac6820e0c0070e4afb9503c
lleyou <61270505+lleyou@users.noreply.github.com>
2022-03-01 22:40:10 +0100

    Validation
    Validation using @annotations in server

--- Server/src/main/java/com/iv1201/server/controller/Competence_profileController.java
+++ Server/src/main/java/com/iv1201/server/controller/Competence_profileController.java
@@ -2,6 +2,7 @@ package com.iv1201.server.controller;
 
 import com.iv1201.server.entity.Competence_profile;
 import com.iv1201.server.service.Competence_profileService;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +18,7 @@ public class Competence_profileController {
     private Competence_profileService service;
     
     @PostMapping("/addProfile")
-    public Competence_profile addProfile(@RequestBody Competence_profile competence_profile){
+    public Competence_profile addProfile(@Valid @RequestBody Competence_profile competence_profile){
         return service.saveCompetence_profile(competence_profile);
     }
 }
